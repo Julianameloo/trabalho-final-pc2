a19ec1bb3734f779c394cc82a87d068e9c76ddb8
Julianameloo <msjulianamelo@gmail.com>
2016-11-16 10:23:48 -0200

    ubuntu 2

--- principal.c
+++ principal.c
@@ -4,6 +4,7 @@ int main () {
 	printf ("Arquivo principal\n");
 	printf ("Alteração teste ubuntu.\n");
 	printf ("Alteração teste ubuntu 2.\n");
+	printf ("Alteração teste ubuntu 3.\n");
 	return 0;
 }
 	
