71be84de1062a385b5746f9e6578eb2d6bf832f5
Julianameloo <msjulianamelo@gmail.com>
2016-11-16 10:45:10 -0200

    -

--- principal.c
+++ principal.c
@@ -4,7 +4,6 @@ int main () {
 	printf ("Arquivo principal\n");
 	printf ("Alteração teste ubuntu.\n");
 	printf ("Alteração teste ubuntu 2.\n");
-	printf ("Alteração teste ubuntu 3.\n");
 	return 0;
 }
 	
